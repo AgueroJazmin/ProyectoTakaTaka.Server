0f7cff57118e3e0e19ae53f396cf7847b76aec5f
AgueroJazmin <jazminaguerotejeda@gmail.com>
2026-04-29 16:33:13 -0300

    Actualizacion

--- ProyectoTakaTaka.Server/ProyectoTakaTaka.Server.Client/Program.cs
+++ ProyectoTakaTaka.Server/ProyectoTakaTaka.Server.Client/Program.cs
@@ -6,5 +6,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
-
+   
 await builder.Build().RunAsync();
